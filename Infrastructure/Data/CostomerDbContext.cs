0ec5b34f8eda84741ad6aac493c179b2607173ae
allan@danline.dk <allan@danline.dk>
2021-03-29 13:58:01 +0200

    Moved DbContext and migrations to Infrastructure

--- Infrastructure/Data/CostomerDbContext.cs
+++ Infrastructure/Data/CostomerDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Domain.Data
+namespace Infrastructure.Data
 {
     public class CostomerDbContext : DbContext
     {
---
+++ Infrastructure/Migrations/20210329115634_DeleteTestTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class DeleteTestTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Test");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Test",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Test", x => x.Id);
+                });
+        }
+    }
+}
