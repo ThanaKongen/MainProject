9a7f77eb217ce6ed53836b2c2e8f0f80bb49ab9d
allan@danline.dk <allan@danline.dk>
2021-03-29 15:26:20 +0200

    Added EFCoreUnitOfWork and IUnitOfWork

---
+++ Infrastructure/Shared/EFCoreUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Shared
+{
+    public class EFCoreUnitOfWork : IUnitOfWork
+    {
+        private readonly Data.CostomerDbContext _dbContext;
+
+        public EFCoreUnitOfWork(Data.CostomerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task Commit() => _dbContext.SaveChangesAsync();
+    }
+}
---
+++ Infrastructure/Shared/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Infrastructure.Shared
+{
+    public interface IUnitOfWork
+    {
+        Task Commit();
+    }
+}
