f47a6fe1be2d38ff9cc1f5873a8c1a8494a3eef7
allan@danline.dk <allan@danline.dk>
2021-03-30 15:13:10 +0200

    Started on repository in infrastructure and created Application layer with interface of repository

---
+++ Application/Command/ICustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using System.Threading.Tasks;
+
+namespace Application.Command
+{
+    public interface ICustomerRepository /*<TEntity> where TEntity : class, new()*/
+    {
+        Task AddAddress(Address Entity);
+        Task AddBusinessCustomer(BusinessCustomer Entity);
+        Task AddPrivateCustomerAsync(PrivateCustomer Entity);
+        Task<bool> AddressExistsAsync(int Id);
+        Task<bool> BusinessCustomerExistsAsync(int Id);
+        Task CustomerExistAsync(Customer Entity);
+        void Dispose();
+        Task<Address> LoadAddressAsync(int Id);
+        Task<BusinessCustomer> LoadBusinessCustomerAsync(int Id);
+        Task<Customer> LoadCustomerAsync(int Id);
+        Task<PrivateCustomer> LoadPrivateCustomerAsync(int Id);
+        Task<bool> PrivateCustomerExistsAsync(int Id);
+    }
+}
---
+++ Infrastructure/CustomerRepository.cs
@@ -0,0 +1,128 @@
+﻿using Infrastructure.Data;
+using System;
+using Domain.Models;
+using System.Threading.Tasks;
+using Application.Command;
+
+namespace Infrastructure
+{
+    //public class CustomerRepository<TEntity> : ICustomerRepository<TEntity> where TEntity : class, new()
+    //{
+    //    protected readonly CostomerDbContext DbContext;
+
+    //    public CustomerRepository(CostomerDbContext dbContext)
+    //    {
+    //        DbContext = dbContext;
+    //    }
+
+    //    public IQueryable<TEntity> GetAll()
+    //    {
+    //        try
+    //        {
+    //            return DbContext.Set<TEntity>();
+    //        }
+    //        catch (Exception ex)
+    //        {
+
+    //            throw new Exception($"Couldn't retrieve entities: {ex.Message}");
+    //        }
+    //    }
+
+    //    public async Task<TEntity> AddAsync(TEntity Entity)
+    //    {
+    //        if (Entity==null)
+    //        {
+    //            throw new ArgumentException($"{nameof(AddAsync)} entity must not be null");
+    //        }
+
+    //        try
+    //        {
+    //            await DbContext.AddAsync(Entity);
+    //            await DbContext.SaveChangesAsync();
+
+    //            return Entity;
+    //        }
+    //        catch (Exception ex)
+    //        {
+
+    //            throw new Exception($"{nameof(Entity)} could not be saved: {ex.Message}");
+    //        }
+    //    }
+
+    //    public async Task<TEntity> UpdateAsync(TEntity Entity)
+    //    {
+    //        if (Entity == null)
+    //        {
+    //            throw new ArgumentException($"{nameof(UpdateAsync)} entity must not be null");
+    //        }
+
+    //        try
+    //        {
+    //            DbContext.Update(Entity);
+    //            await DbContext.SaveChangesAsync();
+
+    //            return Entity;
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw new Exception($"{nameof(Entity)} could not be saved: {ex.Message}");
+    //        }
+    //    }
+
+    //    //public Task<Customer> GetEntityById (int Id)
+    //    //{
+    //    //    return GetAll().FirstOrDefaultAsync(x => x.Id == Id);
+    //    //}
+    //}
+    public class CustomerRepository : ICustomerRepository, IDisposable
+    {
+        private readonly CostomerDbContext DbContext;
+
+        public CustomerRepository(CostomerDbContext _DbContext)
+        {
+            DbContext = _DbContext;
+        }
+
+        public void Dispose() => DbContext.Dispose();
+
+        //Private Customer
+        public async Task AddPrivateCustomerAsync(PrivateCustomer Entity)
+            => await DbContext.PrivateCustomer.AddAsync(Entity);
+
+        public async Task<bool> PrivateCustomerExistsAsync(int Id)
+            => await DbContext.PrivateCustomer.FindAsync(Id) != null;
+
+        public async Task<PrivateCustomer> LoadPrivateCustomerAsync(int Id)
+            => await DbContext.PrivateCustomer.FindAsync(Id);
+
+        //Business Customer
+        public async Task AddBusinessCustomer(BusinessCustomer Entity)
+            => await DbContext.BusinessCustomer.AddAsync(Entity);
+
+        public async Task<bool> BusinessCustomerExistsAsync(int Id)
+            => await DbContext.BusinessCustomer.FindAsync(Id) != null;
+
+        public async Task<BusinessCustomer> LoadBusinessCustomerAsync(int Id)
+            => await DbContext.BusinessCustomer.FindAsync(Id);
+
+
+        //Might change this later
+        public async Task CustomerExistAsync(Customer Entity)
+            => await DbContext.Customer.FindAsync(Entity);
+
+        public async Task<Customer> LoadCustomerAsync(int Id)
+            => await DbContext.Customer.FindAsync(Id);
+
+        //Address
+        public async Task AddAddress(Address Entity)
+            => await DbContext.Address.AddAsync(Entity);
+
+        public async Task<bool> AddressExistsAsync(int Id)
+            => await DbContext.Address.FindAsync(Id) != null;
+
+        public async Task<Address> LoadAddressAsync(int Id)
+            => await DbContext.Address.FindAsync(Id);
+
+        //
+    }
+}
