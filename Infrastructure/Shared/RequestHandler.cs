a3fed4a2b57456ba81068140d8114a3441d2cde3
allan@danline.dk <allan@danline.dk>
2021-03-30 11:26:30 +0200

    Finished RequestHandler

--- Infrastructure/Shared/RequestHandler.cs
+++ Infrastructure/Shared/RequestHandler.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Infrastructure.Shared
 {
     public static class RequestHandler
     {
+        public static async Task<IActionResult> HandleCommand<T>(T request, Func<T, Task> handler, ILogger log)
+        {
+            try
+            {
+                log.Debug("Handling HTTP request of type {type}", typeof(T).Name);
+                await handler(request);
+                return new OkResult();
+            }
+            catch (Exception e)
+            {
+                log.Error(e, "Error handling the command");
+                return new BadRequestObjectResult(new
+                {
+                    error = e.Message,
+                    stackTrace = e.StackTrace
+                });
+            }
+        }
 
+        public static async Task<IActionResult> HandleQuery<T>(Func<Task<T>> handler, ILogger log)
+        {
+            try
+            {
+                return new OkObjectResult(await handler());
+            }
+            catch (Exception e)
+            {
+                log.Error(e, "Error handling the query");
+                return new BadRequestObjectResult(new
+                {
+                    error = e.Message,
+                    stackTrace = e.StackTrace
+                });
+            }
+        }
     }
 }
